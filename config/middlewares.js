c772a9c436187496851b8de04aaa4dc0f72ce0e8
Alex Ovalle <alexova94@gmail.com>
2026-04-05 13:19:39 -0500

    chore: initial commit - strapi backend setup

---
+++ config/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'ecommerce-strapi-games-proj.s3.us-west-2.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'ecommerce-strapi-games-proj.s3.us-west-2.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+];
---
+++ config/plugins.js
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                        secretAccessKey: env('AWS_ACCESS_SECRET'),
+                    },
+                    region: env('AWS_REGION'),
+                    params: {
+                        Bucket: env('AWS_BUCKET'),
+                        ACL: env('AWS_ACL', 'public-read'),
+                        signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+                    },
+                },
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+});
